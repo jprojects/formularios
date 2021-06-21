adcbb14cd44e4d51705493b89a2db8453c30cb8e
foxyframework <kim+foxy@aficat.com>
2021-06-21 09:55:16 +0200

    sql changes for installs and updates

--- administrator/sql/update.mysql.utf8.sql
+++ administrator/sql/update.mysql.utf8.sql
@@ -1,1 +1,7 @@
-/* Only premium users are allowed to update a component */
+ALTER TABLE `#__formularios_fields` ADD `field_msg_success` VARCHAR(150) NOT NULL DEFAULT 'COM_FORMULARIOS_VALIDATION_SUCCESS_MSG' AFTER `field_long`;
+
+ALTER TABLE `#__formularios_fields` ADD `field_msg_error` VARCHAR(150) NOT NULL DEFAULT 'COM_FORMULARIOS_VALIDATION_ERROR_MSG' AFTER `field_msg_success`;
+
+ALTER TABLE `#__formularios_fields` ADD `field_readonly` TINYINT(1) NOT NULL DEFAULT 0 AFTER `field_msg_error`;
+
+ALTER TABLE `#__formularios_fields` ADD `field_disabled` TINYINT(1) NOT NULL DEFAULT 0 AFTER `field_readonly`;
